3c5bc22ac6cb2a7e0175453ffbcf6d1609bdc9f6
Anabel <ajimenezm@edu.tecnocampus.cat>
2021-10-18 16:08:00 +0200

    :)

--- Assets/Scripts/State/S_Patrol.cs
+++ Assets/Scripts/State/S_Patrol.cs
@@ -6,7 +6,7 @@ public class S_Patrol : I_State
 {
 
     StateController enemy;
-    [SerializeField] ScriptableObject_Target _targetData;
+ 
     
 
     public void startState(StateController enemy)
