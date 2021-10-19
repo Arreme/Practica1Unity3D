3652582d966c2841b42e521a35dd5d502fdea103
arreme <a.reme@hotmail.com>
2021-10-19 17:49:19 +0200

    Ui and shield done

--- Assets/Scripts/AmmoUI.cs
+++ Assets/Scripts/AmmoUI.cs
@@ -9,10 +9,22 @@ public class AmmoUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TextMeshProUGUI text2;
 
+    [SerializeField] private Image _health;
+    [SerializeField] private Image _shield;
+
     public void updateAmmo(int loadedBullets, int unloadedBullets)
     {
         text.text = loadedBullets.ToString() + "/" + unloadedBullets.ToString();
         text2.text = text.text;
+    }
 
+    public void updateHealth(float health)
+    {
+        _health.fillAmount = health / 100;
+    }
+
+    public void updateShield(float shield)
+    {
+        _shield.fillAmount = shield / 100;
     }
 }
--- Assets/Scripts/PlayerHealthSysem.cs
+++ Assets/Scripts/PlayerHealthSysem.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealthSysem : MonoBehaviour
 {
     [SerializeField] float _health;
-    [SerializeField] float shield;
+    [SerializeField] float _shield;
+
+    [SerializeField] private UnityEvent<float> _healthEvent;
+    [SerializeField] private UnityEvent<float> _shieldEvent;
 
     public void getHit()
     {
-        if (shield > 0)
+        if (_shield > 0)
         {
-            shield -= 75;
-            //Update UI
+            _shield -= 75;
+            Debug.Log(_shield);
+            _shield = _shield < 0 ? 0 : _shield;
+            _shieldEvent.Invoke(_shield);
         } else
         {
             _health -= 25;
-            //Update UI
+            _health = _health < 0 ? 0 : _health;
+            _healthEvent.Invoke(_health);
             if (_health <= 0)
             {
                 //DeathScreen
             }
         }
-        
     }
 }
